27a5c23a62f4d272090ccaff00894fe917cfd7fd
EmmanuelN-L <46698046+EmmanuelN-L@users.noreply.github.com>
2020-10-21 21:54:10 -0400

    Implementted Bullet Controller, Enemy controller, Player Controller
    Changed the 3 classes scripts for Landscape implementation. Edited the transform for the player, enemy and bullet prefabs. To face the correct way.

--- Assets/_Scripts/BulletController.cs
+++ Assets/_Scripts/BulletController.cs
@@ -6,6 +6,8 @@ public class BulletController : MonoBehaviour, IApplyDamage
 {
     public float verticalSpeed;
     public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
     public BulletManager bulletManager;
     public int damage;
     
@@ -19,17 +21,26 @@ public class BulletController : MonoBehaviour, IApplyDamage
     void Update()
     {
         _Move();
-        _CheckBounds();
+        _CheckBoundsHorizontal();
+        //_CheckBounds();
     }
-
+    //Bullets will move horizontally instead of vertically
     private void _Move()
     {
-        transform.position += new Vector3(0.0f, verticalSpeed, 0.0f) * Time.deltaTime;
+        transform.position += new Vector3(horizontalSpeed, verticalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         if (transform.position.y > verticalBoundary)
+        {
+            bulletManager.ReturnBullet(gameObject);
+        }
+    }
+
+    private void _CheckBoundsHorizontal()
+    {
+        if (transform.position.x > horizontalBoundary)
         {
             bulletManager.ReturnBullet(gameObject);
         }
